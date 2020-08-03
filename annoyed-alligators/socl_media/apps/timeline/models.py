64759b5fc6b773b281378df231a517c94d481662
Nishith Showri <58651995+nishithshowri006@users.noreply.github.com>
2020-08-03 19:31:20 +0530

    Made asked changes

--- annoyed-alligators/socl_media/apps/timeline/models.py
+++ annoyed-alligators/socl_media/apps/timeline/models.py
@@ -5,13 +5,10 @@ from django.contrib.auth.models import User
 
 
 class Posts(models.Model):
-    post_title = models.CharField(max_length=40, verbose_name="title")
     post_content = models.TextField(verbose_name="content")
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_date_published = models.DateTimeField(auto_now_add=True,
+    post_date_posted = models.DateTimeField(auto_now_add=True,
                                                verbose_name="Published on")
-    post_last_edited = models.DateTimeField(auto_now=True,
-                                            verbose_name="Last Edited")
 
     def __str__(self):
         return self.post_title
